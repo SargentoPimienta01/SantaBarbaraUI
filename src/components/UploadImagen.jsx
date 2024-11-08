36142775affc67cbfc6c7d6f524fef45b5bd99b2
SargentoPimienta01 <cookiegpjr@gmail.com>
2024-11-08 10:04:20 -0400

    Brutalist Upload V1

--- src/components/UploadImagen.jsx
+++ src/components/UploadImagen.jsx
@@ -54,48 +54,80 @@ const ImageUploader = () => {
     };
 
     return (
-        <div className="relative flex justify-center items-center flex-col gap-4 w-full max-w-md mx-auto p-6 border-2 border-dashed border-gray-400 rounded-lg transition-colors duration-300 ease-in-out hover:border-blue-500 bg-white shadow-md">
-            <div
-                className={`upload-area w-full p-6 text-center rounded-lg transition ${
-                    isDragging ? 'bg-blue-100 border-blue-500' : ''
-                }`}
-                onDragOver={handleDragOver}
-                onDragLeave={handleDragLeave}
-                onDrop={handleDrop}
-            >
-                {!selectedImage ? (
-                    <>
-                        <p className="text-gray-700 font-medium">Arrastra y suelta una imagen aquí o</p>
-                        <label
-                            htmlFor="file-upload"
-                            className="mt-2 inline-block bg-gradient-to-r from-blue-600 to-blue-500 text-white py-2 px-4 rounded cursor-pointer shadow-lg transition hover:from-blue-700 hover:to-blue-600"
-                        >
-                            Selecciona una imagen
-                        </label>
-                        <input
-                            id="file-upload"
-                            type="file"
-                            accept="image/*"
-                            onChange={handleFileChange}
-                            className="hidden"
-                        />
-                    </>
-                ) : (
-                    <div className="preview flex flex-col items-center">
-                        <img
-                            src={selectedImage}
-                            alt="Vista previa de la imagen cargada"
-                            className="mt-4 w-full h-auto rounded-lg shadow-lg border"
-                        />
-                        <button
-                            onClick={handleRemoveImage}
-                            className="mt-4 bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 shadow-lg transition-colors duration-200"
-                        >
-                            Eliminar
-                        </button>
-                    </div>
-                )}
+        <div className="image-upload-container">
+            <div className="marquee bg-white">
+                <div className="marquee-content">
+                    <span>Coman</span>
+                    <span>Mas</span>
+                    <span>Pollo</span>
+                    <span></span>
+                    <span>-</span>
+                    <span></span>
+                    <span>Coman</span>
+                    <span>Menos</span>
+                    <span>Pollo</span>
+                </div>
             </div>
+            <div className="nb-header">
+                <h1 className="nb-title">Carga y Previsualización de Imágenes</h1>
+                <div div className="nb-datos">
+                    <input className="nb-input blue" placeholder="Incubadora" />
+                    <input className="nb-input blue" placeholder="Maple" />
+                </div>
+            </div>
+
+            <div className="nb-carga">
+                <div className="relative flex justify-center items-center flex-col gap-4 w-full max-w-md mx-auto p-6 border-2 border-dashed border-gray-400 rounded-lg transition-colors duration-300 ease-in-out hover:border-blue-500 bg-white shadow-md">
+                    <div
+                        className={`upload-area w-full p-6 text-center rounded-lg transition ${
+                            isDragging ? 'bg-blue-100 border-blue-500' : ''
+                        }`}
+                        onDragOver={handleDragOver}
+                        onDragLeave={handleDragLeave}
+                        onDrop={handleDrop}
+                    >
+                        {!selectedImage ? (
+                            <>
+                                
+                                <label
+                                    htmlFor="file-upload"
+                                    className="mt-2 inline-block bg-gradient-to-r from-blue-600 to-blue-500 text-white py-2 px-4 rounded cursor-pointer shadow-lg transition hover:from-blue-700 hover:to-blue-600"
+                                >
+                                    Selecciona una imagen
+                                </label>
+                                <input
+                                    id="file-upload"
+                                    type="file"
+                                    accept="image/*"
+                                    onChange={handleFileChange}
+                                    className="hidden"
+                                />
+                            </>
+                        ) : (
+                            <div className="preview flex flex-col items-center">
+                                <img
+                                    src={selectedImage}
+                                    alt="Vista previa de la imagen cargada"
+                                    className="mt-4 w-full h-auto rounded-lg shadow-lg border"
+                                />
+                                <button
+                                    onClick={handleRemoveImage}
+                                    className="mt-4 bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 shadow-lg transition-colors duration-200"
+                                >
+                                    Eliminar
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                </div>
+                <p className="nb-texto">Cuando subes una imagen de los huevos, estás iniciando un proceso avanzado que utiliza inteligencia artificial para evaluar su viabilidad. La tecnología detrás de este análisis combina dos redes neuronales diseñadas específicamente para interpretar los datos visuales de manera precisa y confiable.
+                Primero, la imagen pasa por una red neuronal convolucional encargada de preprocesar y segmentar cada huevo en la imagen. Esta etapa asegura que cada huevo sea detectado individualmente, independientemente de su posición o agrupación, y que los datos visuales relevantes se aíslen para un análisis efectivo.
+                A continuación, la imagen procesada pasa a una segunda red neuronal, especializada en analizar la colorimetría y otros factores visuales de cada huevo. Esta red está entrenada para reconocer patrones específicos en la variante de huevo Cobb 500, que es conocida por sus características de alta eficiencia en la industria avícola. La Cobb 500 
+                es una variedad que, con un manejo adecuado, tiende a tener tasas elevadas de fertilidad y crecimiento, pero también puede presentar ciertos desafíos de viabilidad que es crucial identificar a tiempo para asegurar la calidad en la incubación.
+                Al completar este proceso, el sistema genera un análisis que muestra el porcentaje de inviabilidad de cada huevo basado en su color, textura, y otros factores internos detectados en la imagen. Este resultado te permite tomar decisiones informadas y proactivas sobre el manejo de los huevos y su potencial para incubación.</p>
+            </div>
+
+            
         </div>
     );
 };
